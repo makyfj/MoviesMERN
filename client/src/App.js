dbf62bb906a980b618821e56d67e8ca693b89e8d
Maky <franklinjaradev@gmail.com>
2021-05-23 23:24:19 -0400

    Added components and material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import movies from "./images/movies.svg";
+import Movies from "./components/Movies/Movies";
+import Form from "./components/Form/Form";
 
 const App = () => {
-  return <div>Movies</div>;
+  return (
+    <>
+      <Container maxWidth="lg">
+        <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">
+            Movies
+          </Typography>
+          <img src={movies} alt="movies" height="80" />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justify="space-between"
+              alignItems="stretch"
+              spacing={3}
+            >
+              <Grid item xs={12} sm={7}>
+                <Movies />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/components/Movies/Movies.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Movie from "./Movie/Movie";
+
+const Movies = () => {
+  return (
+    <>
+      <h1>MOVIES</h1>
+      <Movie />
+      <Movie />
+    </>
+  );
+};
+
+export default Movies;
